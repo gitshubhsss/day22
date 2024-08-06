bcc0ae3ffd99bfa8e887c019b4e6cabac06e6aa7
gitshubhsss <shubhamranjane16@gmail.com>
2024-08-06 22:21:12 +0530

    .\ElectricCar.java

---
+++ practice/ElectricCar.java
@@ -0,0 +1,19 @@
+class Automobile{
+    private String drive(){
+        return "driving vehicle";
+    }
+}
+class Car extends Automobile{
+    protected String drive(){
+        return "driving car";
+    }
+}
+
+public class ElectricCar{
+    
+}
+public class q3 {
+    public static void main(String[] args) {
+        
+    }
+}
