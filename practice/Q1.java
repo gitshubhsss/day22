c9e05c6a2523d9ec24896218cb3bd97dd0972e79
gitshubhsss <shubhamranjane16@gmail.com>
2024-08-06 22:21:49 +0530

    .\Q1.java

---
+++ practice/Q1.java
@@ -0,0 +1,12 @@
+class Student{
+    String name;
+    int roll;
+}
+
+public class Q1 {
+    public static void main(String[] args) {
+        Student c=new Student();
+        c.name="atul";
+        System.out.println(c.name);
+    }
+}
